c5ccc831248966fbe436afdf26d417417be43349
Jobir <toshtemirovjobir6@gmail.com>
2024-09-07 13:11:49 +0500

    added some files and func

--- Auth/auth.py
+++ Auth/auth.py
@@ -10,6 +10,32 @@ class Auth:
         self.created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S").__str__()
         self.__database = Database()
 
+
+    
+    @log_decorator
+    def register(self):
+        """
+                Register a new user by adding their details to the users table.
+        """
+        first_name: str = input("First Name: ")
+        last_name: str = input("Last Name: ")
+        email: str = input("Email: ")
+        password: str = hashlib.sha256(input("Password: ").strip().encode('utf-8')).hexdigest()
+        confirm_password: str = hashlib.sha256(input("Confirm password: ").strip().encode('utf-8')).hexdigest()
+        while password != confirm_password:
+            print("Passwords do not match")
+            password: str = hashlib.sha256(input("Password: ").strip().encode('utf-8')).hexdigest()
+            confirm_password: str = hashlib.md5(input("Confirm password: ").strip().encode('utf-8')).hexdigest()
+        query = '''
+               INSERT INTO "users" (FIRSTNAME, LASTNAME, EMAIL, PASSWORD)
+               VALUES (%s, %s, %s, %s);
+               '''
+        params = (first_name, last_name, email, password)
+        with self.__database as db:
+            db.execute(query, params)
+        print("User registered successfully")
+        return None
+
     @log_decorator
     def login(self):
 
