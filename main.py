13480df4cb143acff90195810e68fb171389928e
Jobir <toshtemirovjobir6@gmail.com>
2024-09-07 12:29:00 +0500

    created .vnv folder database.ini file, auth folder auth.py file, database folder config and db-setting file,decorator folder decorator file, creted auth menu and someone else

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-def menu():  
-    pass
+def auth_menu():
+    text = '''
+1. Login
+2. Logout
+    '''
+    print(text)
+    try:
+        user_input: int = int(input("Choose menu: "))
+        if user_input == 1:
+            result_login = auth.login()
+            if not result_login['is_login']:
+                auth_menu()
+            elif result_login['role'] == 'super_admin':
+                pass
+            elif result_login['role'] == 'user':
+                pass
+            auth_menu()
+        elif user_input == 2:
+            print("Good bye!")
+            Auth.logout()
+        else:
+            print("Invalid input")
+            auth_menu()
+    except Exception as e:
+        print(f'Error: {e}')
+        auth_menu()
