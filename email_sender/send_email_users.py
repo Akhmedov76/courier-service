24dbb1a3b6ad30e5ed6675d39988db7a2d70559b
Jobir <toshtemirovjobir6@gmail.com>
2024-09-09 13:47:45 +0500

    changes

--- email_sender/send_email_users.py
+++ email_sender/send_email_users.py
@@ -1,14 +1,12 @@
 import threading
-
 from database_config.db_settings import execute_query
 from decorator.decorator import log_decorator
 from email_sender.email import send_mail
 
 
 class EmailSendMessage:
-    import threading
 
-    import threading
+    
 
     @log_decorator
     def send_email_all_users(self):
